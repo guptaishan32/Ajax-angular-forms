3506da31a213975caa05d471394eeeee9be48271
guptaishan32 <44001710+guptaishan32@users.noreply.github.com>
2018-10-12 16:27:52 +0530

    Initial commit

--- connection.php
+++ connection.php
@@ -2,9 +2,6 @@
 
     $dbcon=mysqli_connect("localhost","root","root","racw3080");
    /* $dbcon=mysqli_connect("server_name","name","password","database_name");*/
-/*    $dbcon=mysqli_connect("mysql.hostinger.in","u829129176_rishu","R@cw3080@","u829129176_rotaa");*/
-/*    $dbcon=mysqli_connect("localhost","id6484559_guptaishan32","@shugupt@","id6484559_ishan");*/
-
    /* if(mysqli_connect_errno()==0)
         echo "Connected";
     else
